c82192e7196752a56a943474da33783b221d06b1
humanoidkiwifruit <248161851+humanoidkiwifruit@users.noreply.github.com>
2026-08-15 16:48:59 +0800

    Add files via upload

--- robot.py
+++ robot.py
@@ -5,9 +5,14 @@
 # the WPILib BSD license file in the root directory of this project.
 
 
-# NO AUTONOMOUS - AFTER TALKING TO ARCHIE
+
+# new stuff: stop/start_shooting(), choosing modes, moved polling, A button to move forwards.
+
+# pylint: disable=pointless-string-statement, attribute-defined-outside-init, missing-function-docstring, missing-module-docstring, missing-class-docstring
+
 import wpilib
 import wpilib.drive
+from wpilib import SendableChooser, SmartDashboard
 import rev
 from ntcore import NetworkTableInstance
 
@@ -15,25 +20,35 @@ from ntcore import NetworkTableInstance
 
 class MyRobot(wpilib.TimedRobot):
 
-    def rt_pressed(self):
-        #print("rt_pressed")
-        self.left_shooting_motor.set(-self.shooting_speed)
+    def start_blurting_balls(self):
+        self.left_shooting_motor.set(self.shooting_speed)
         self.right_shooting_motor.set(self.shooting_speed)
 
-    def rt_released(self):
-        #print("rt_released")
+    def stop_shooting_motors(self):
         self.left_shooting_motor.set(0)
         self.right_shooting_motor.set(0)
 
+    def start_shooting(self):
+        self.left_shooting_motor.set(-self.shooting_speed)
+        self.right_shooting_motor.set(self.shooting_speed)
+
+    def rt_pressed(self):
+        #print("rt_pressed")
+        self.start_shooting()
+
+    def rt_released(self):
+        #print("rt_released")
+        self.stop_shooting_motors()
+
     def lt_pressed(self):
         #print("lt_pressed")
+        #motors neetd to go bw to suck balls
         self.left_shooting_motor.set(-self.shooting_speed)
         self.right_shooting_motor.set(-self.shooting_speed)
 
     def lt_released(self):
         #print("lt_released")
-        self.left_shooting_motor.set(0)
-        self.right_shooting_motor.set(0)
+        self.stop_shooting_motors()
 
 
     def rb_pressed(self):
@@ -59,6 +74,16 @@ class MyRobot(wpilib.TimedRobot):
 
 
     def robotInit(self):
+
+        self.auto_chooser = SendableChooser()
+        self.auto_chooser.setDefaultOption("do_nothing", "do_nothing")
+        self.auto_chooser.addOption("forward_and_shoot", "forward_and_shoot")
+        self.auto_chooser.addOption("oli_auto", "oli_auto")
+        self.auto_chooser.addOption("shoot", "shoot")
+        self.auto_chooser.addOption("half_sec_back_and_shoot", "half_sec_back_and_shoot")
+        
+        SmartDashboard.putData("Autonomous Chooser", self.auto_chooser)
+
 
         self.timer = wpilib.Timer()
         #print("robotInit (robot initialisation function)")
@@ -105,11 +130,19 @@ class MyRobot(wpilib.TimedRobot):
         self.rb_event = self.driverController.rightBumper(self.loop)
         self.lb_event = self.driverController.leftBumper(self.loop)
 
+        self.b_event = self.driverController.B(self.loop)
+
+
+
         self.rt_event.rising().ifHigh(lambda: self.rt_pressed())
         self.rt_event.falling().ifHigh(lambda: self.rt_released())
 
         self.lt_event.rising().ifHigh(lambda: self.lt_pressed())
         self.lt_event.falling().ifHigh(lambda: self.lt_released())
+
+        self.b_event.rising().ifHigh(lambda: self.start_blurting_balls())
+        self.b_event.falling().ifHigh(lambda: self.stop_shooting_motors())
+        
 
         #self.rb_event.rising().ifHigh(lambda: self.rb_pressed())
         #self.rb_event.falling().ifHigh(lambda: self.rb_released())
@@ -120,16 +153,14 @@ class MyRobot(wpilib.TimedRobot):
 
 
     def robotPeriodic(self):
-        pass
         self.loop.poll()
+        pass
         #self.num_pub.set(wpilib.RobotController.getBatteryVoltage())
 
     def teleopInit(self):
         #print("entering teleopInit (teleoperation/remote control initialisation function)")
+        self.stop_shooting_motors()
         self.robotDrive.arcadeDrive(0, 0)
-        self.right_shooting_motor.set(0)
-        self.left_shooting_motor.set(0)
-        pass
 
     def teleopPeriodic(self):
         # Drive with tank drive.
@@ -140,7 +171,11 @@ class MyRobot(wpilib.TimedRobot):
         left_bumper_down = self.driverController.getLeftBumper()
         right_bumper_down = self.driverController.getRightBumper()
         #loop poll was here before
-        if left_bumper_down == right_bumper_down: #no bumpers OR both bumpers: normal tank drive
+        if self.driverController.getAButton():
+            print("A button is down")
+            self.robotDrive.arcadeDrive(1, 0, squareInputs=False)
+
+        elif left_bumper_down == right_bumper_down: #no bumpers OR both bumpers: normal tank drive
             self.robotDrive.tankDrive(
                 -self.driverController.getLeftY(), -self.driverController.getRightY()
             )
@@ -152,68 +187,91 @@ class MyRobot(wpilib.TimedRobot):
             self.robotDrive.arcadeDrive(0, -1.0, squareInputs=False)
             #self.left_motor_group.set(1)
             #self.right_motor_group.set(-1)
-        else:
+        else: #Something weird
             self.robotDrive.tankDrive(
                 -self.driverController.getLeftY(), -self.driverController.getRightY()
             )
-            #self.left_motor_group.set(0)
-            #self.right_motor_group.set(0)
 
-        #self.loop.poll()
+        # self.loop.poll()
 
 
         if self.driverController.getRightTriggerAxis() > self.shootThreshold:
             pass
+            #DO NOT REMOVE FOLLOWING COMMENT:
             #print("shoot the cannon pow pow")
-            #motor.set(0.5)
-            #self.right_shooting_motor.set(0.5)
-            #self.left_shooting_motor.set(0.5)
 
     def autonomousInit(self):
         self.timer.restart()
+        self.selected_auto = self.auto_chooser.getSelected()
         #print("Entering autonomousInit (autonomous period initialisation function)")
-        #start far right
-        #forward under trench
-        #turn to balls
-        #pick up balls
-        #turn
-        #shoot
 
     def autonomousPeriodic(self):
-        if self.timer.get() < 19:
-            #print("autonomousPeriodic")
-            self.left_shooting_motor.set(self.shooting_speed)
-            self.right_shooting_motor.set(self.shooting_speed)
+        #print("autonomousPeriodic")
+        if self.selected_auto == "oli_auto":
+            print("oli auto")
+
+            if self.timer.get() < 1.5:
+                self.robotDrive.arcadeDrive(0.5, 0, squareInputs=False)
+
+            elif self.timer.get() > 1.5 and self.timer.get() < 2:
+                self.robotDrive.arcadeDrive(0, -0.3, squareInputs=False)
+
+            elif self.timer.get() > 2 and self.timer.get() < 3:
+                self.robotDrive.arcadeDrive(0.5, 0, squareInputs=False)
+
+            elif self.timer.get() > 3 and self.timer.get() < 3.5:
+                self.robotDrive.arcadeDrive(0, -0.3, squareInputs=False)
+            
+            elif self.timer.get() > 3.5 and self.timer.get() < 4:
+                self.robotDrive.arcadeDrive(0, -0.3, squareInputs=False)
+
+            elif self.timer.get() > 4 and self.timer.get() < 5:
+                self.robotDrive.arcadeDrive(-1, 0, squareInputs=False)
+
+            elif self.timer.get() > 5 and self.timer.get() < 15:
+                self.start_shooting()
+
+            else:
+                self.robotDrive.stopMotor()  # Stop robot
+                self.stop_shooting_motors()
+
+
+        elif self.selected_auto == "do_nothing":
+            print("do_nothing")
             self.robotDrive.arcadeDrive(0, 0)
-            self.robotDrive.stopMotor()
+            self.stop_shooting_motors()
 
+
+        elif self.selected_auto == "forward_and_shoot":
+            if self.timer.get() > 19:
+                print("forward_and_shoot")
+                self.robotDrive.arcadeDrive(1, 0, squareInputs=False)
+                self.start_shooting()
+            else:
+                self.robotDrive.stopMotor()
+                self.stop_shooting_motors()
+
+
+        elif self.selected_auto == "shoot":
+            if self.timer.get() > 19:
+                print("shoot")
+                self.start_shooting()
+            else:
+                self.robotDrive.stopMotor()
+                self.stop_shooting_motors()
+
+
+        elif self.selected_auto == "half_sec_back_and_shoot":
+            #ROBOT NEEDS TO BE BACKWARDS
+            if self.timer.get() < 0.5:
+                self.robotDrive.arcadeDrive(0.7, 0)
+                self.start_shooting()
+            elif self.timer.get() < 19:
+                self.start_shooting()
+                self.robotDrive.stopMotor()
+            else:
+                self.stop_shooting_motors()
+                self.robotDrive.stopMotor()
         else:
-            self.right_shooting_motor.set(0)
-            self.left_shooting_motor.set(0)
-        '''if self.timer.get() < 1.5:
-            self.robotDrive.arcadeDrive(0.5, 0, squareInputs=False)
-
-        elif self.timer.get() > 1.5 and self.timer.get() < 2:
-            self.robotDrive.arcadeDrive(0, -0.3, squareInputs=False)
-
-        elif self.timer.get() > 2 and self.timer.get() < 3:
-            self.robotDrive.arcadeDrive(0.5, 0, squareInputs=False)
-
-        elif self.timer.get() > 3 and self.timer.get() < 3.5:
-            self.robotDrive.arcadeDrive(0, -0.3, squareInputs=False)
-        
-        elif self.timer.get() > 3.5 and self.timer.get() < 4:
-            self.robotDrive.arcadeDrive(0, -0.3, squareInputs=False)
-
-        elif self.timer.get() > 4 and self.timer.get() < 5:
-            self.robotDrive.arcadeDrive(-1, 0, squareInputs=False)
-
-        elif self.timer.get() > 5 and self.timer.get() < 15:
-            self.left_shooting_motor.set(self.shooting_speed)
-            self.right_shooting_motor.set(self.shooting_speed)
+            print("wrong selection: following else block")
             self.robotDrive.arcadeDrive(0, 0)
-
-        else:
-            self.robotDrive.stopMotor()  # Stop robot
-            self.right_shooting_motor.set(0)
-            self.left_shooting_motor.set(0)'''
